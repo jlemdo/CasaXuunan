3d73e69cf4a89d95505e1ac6d8853ea9acb42db1
jlemdo <jlelpella@gmail.com>
2024-11-19 12:52:54 -0600

    About mod

--- includes/templates/about.php
+++ includes/templates/about.php
@@ -24,7 +24,7 @@
                         <div class="col-lg-3 col-6">
                             <img src="<?php echo BASE_URL; ?>/images/misc/2.jpg" alt="" class="img-responsive wow fadeInUp" data-wow-duration="1.5s">
                         </div>
-                        
+
                         <div class="col-lg-6 wow fadeIn">
                             <div class="padding20">
                                 <h2 class="title mb10">La Experiencia de Lujo<br>Que Recordarás
@@ -37,7 +37,7 @@
                             </div>
                         </div>
 
-                        
+
                         <div class="clearfix"></div>
                     </div>
 
@@ -53,104 +53,117 @@
                     </div>
 
                     <div class="row">
-
                         <div class="col-md-4">
                             <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/restaurant-svgrepo-com.svg" alt=""></span>
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/nature-svgrepo-com.svg" alt="Conexión con la naturaleza">
+                                </span>
                                 <div class="text">
-                                    <h3 class="style-1">Restaurante</h3>
-                                    <p>Disfruta de una experiencia gastronómica única con platos locales e internacionales en nuestro restaurante.</p>
+                                    <h3 class="style-1">Conexión con la Naturaleza</h3>
+                                    <p>Sumérgete en la serenidad de nuestros jardines exuberantes y áreas verdes, diseñadas para ofrecerte una experiencia armoniosa con el entorno natural de Valladolid.</p>
                                 </div>
                             </div>
                         </div>
-                    
+
                         <div class="col-md-4">
                             <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/swimming-pool-svgrepo-com.svg" alt=""></span>
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/hospitality-svgrepo-com.svg" alt="Calidez humana">
+                                </span>
+                                <div class="text">
+                                    <h3 class="style-1">Calidez Humana</h3>
+                                    <p>Disfruta de la hospitalidad y atención personalizada de nuestro equipo, comprometido en hacer de tu estancia una experiencia memorable y acogedora.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="box-icon">
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/comfort-svgrepo-com.svg" alt="Confort">
+                                </span>
+                                <div class="text">
+                                    <h3 class="style-1">Confort</h3>
+                                    <p>Nuestras habitaciones están equipadas con todas las comodidades modernas, incluyendo aire acondicionado, Wi-Fi gratuito y camas de alta calidad para garantizar tu descanso.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="box-icon">
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/tranquility-svgrepo-com.svg" alt="Tranquilidad">
+                                </span>
+                                <div class="text">
+                                    <h3 class="style-1">Tranquilidad</h3>
+                                    <p>Encuentra un oasis de paz en nuestras instalaciones, ideales para relajarte y desconectar del ajetreo diario, disfrutando de la serenidad que ofrece Valladolid.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="box-icon">
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/culinary-svgrepo-com.svg" alt="Experiencia Gastronómica">
+                                </span>
+                                <div class="text">
+                                    <h3 class="style-1">Experiencia Gastronómica</h3>
+                                    <p>Saborea la auténtica cocina yucateca en nuestro restaurante, donde cada platillo es preparado con ingredientes frescos y locales para deleitar tu paladar.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="box-icon">
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/pool-svgrepo-com.svg" alt="Piscina">
+                                </span>
                                 <div class="text">
                                     <h3 class="style-1">Piscina</h3>
-                                    <p>Relájate y refréscate en nuestra piscina rodeada de hermosos jardines tropicales.</p>
+                                    <p>Refresca tus sentidos en nuestra piscina al aire libre, rodeada de naturaleza, perfecta para relajarte después de explorar los encantos de Valladolid.</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="col-md-4">
                             <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/fitness-gym-svgrepo-com.svg" alt=""></span>
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/wifi-svgrepo-com.svg" alt="Wi-Fi Gratuito">
+                                </span>
                                 <div class="text">
-                                    <h3 class="style-1">Área Fitness</h3>
-                                    <p>Mantente en forma durante tu estancia en nuestro gimnasio totalmente equipado.</p>
+                                    <h3 class="style-1">Wi-Fi Gratuito</h3>
+                                    <p>Mantente conectado con nuestro servicio de Wi-Fi gratuito en todas las áreas del hotel, para que puedas compartir tus experiencias y mantenerte al día.</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="col-md-4">
                             <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/coffee-table-svgrepo-com.svg" alt=""></span>
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/parking-svgrepo-com.svg" alt="Estacionamiento Privado">
+                                </span>
                                 <div class="text">
-                                    <h3 class="style-1">Mini Bar</h3>
-                                    <p>Todas nuestras habitaciones cuentan con minibar para tu comodidad.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4" >
-                            <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/meeting-explain-svgrepo-com.svg" alt=""></span>
-                                <div class="text">
-                                    <h3 class="style-1">Sala de Reuniones</h3>
-                                    <p>Espacios modernos y equipados para tus eventos y reuniones de negocios.</p>
+                                    <h3 class="style-1">Estacionamiento Privado</h3>
+                                    <p>Ofrecemos estacionamiento privado y seguro para tu vehículo, brindándote comodidad y tranquilidad durante tu estancia.</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="col-md-4">
                             <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/laundry-machine-svgrepo-com.svg" alt=""></span>
+                                <span class="icon bg-color">
+                                    <img src="<?php echo BASE_URL; ?>/images/svg/laundry-svgrepo-com.svg" alt="Servicio de Lavandería">
+                                </span>
                                 <div class="text">
                                     <h3 class="style-1">Servicio de Lavandería</h3>
-                                    <p>Ofrecemos servicio de lavandería para que siempre estés impecable.</p>
+                                    <p>Para tu conveniencia, contamos con servicio de lavandería, asegurando que siempre tengas ropa limpia y fresca durante tu visita.</p>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="col-md-4">
-                            <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/screen-tv-svgrepo-com.svg" alt=""></span>
-                                <div class="text">
-                                    <h3 class="style-1">TV Satelital</h3>
-                                    <p>Disfruta de una amplia variedad de canales internacionales en tu habitación.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/safebox-svgrepo-com.svg" alt=""></span>
-                                <div class="text">
-                                    <h3 class="style-1">Caja Fuerte</h3>
-                                    <p>Tus pertenencias estarán seguras en la caja fuerte disponible en cada habitación.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="box-icon">
-                                <span class="icon bg-color"><img src="<?php echo BASE_URL; ?>/images/svg/car-parking-svgrepo-com.svg" alt=""></span>
-                                <div class="text">
-                                    <h3 class="style-1">Estacionamiento</h3>
-                                    <p>Contamos con estacionamiento privado para nuestros huéspedes.</p>
-                                </div>
-                            </div>
-                        </div>
-                        
                     </div>
+
 
                 </div>
             </section>
 
             <!-- subheader close -->
-
-
-
-
